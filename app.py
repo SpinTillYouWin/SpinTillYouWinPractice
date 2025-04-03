8322a289747832075d6ae27b4d69c36d77fe66f4
Yusuf Oz <ysforce1@users.noreply.huggingface.co>
2025-04-03 16:31:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import gradio as gr
+import pandas as pd
+import json
+from itertools import combinations
+import random
+
 # European Roulette wheel order
 WHEEL_EUROPEAN = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23,
                   10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
