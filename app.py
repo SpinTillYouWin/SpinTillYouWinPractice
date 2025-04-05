19780b04bc2e24040be01b3fbce9c6ba038ed389
Yusuf Ozusen <ysforce1@users.noreply.huggingface.co>
2025-04-05 14:44:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from itertools import combinations
 import random
+from roulette_data import *
 
 # European Roulette wheel order
 WHEEL_EUROPEAN = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23,
